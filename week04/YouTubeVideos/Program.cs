6280c16c71ed47274585aea01004f754229b4a65
thneri95 <tiago.neri1995@outlook.com>
2025-05-25 14:15:04 -0300

    W04 youtube concluded

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -8,17 +8,17 @@ class Program
 
         List<Video> videos = new List<Video>();
 
-        Video video1 = new Video("The Restoration of the Gospel", "The Church of Jesus Christ", 420);
+        Video video1 = new Video("The Restoration of the Gospel", "The Church of Jesus Christ of Latter-day Saints", 420);
         video1.AddComment(new Comment("Emma", "This video brings peace to my heart"));
         video1.AddComment(new Comment("Joseph", "So inspiring! I felt the Spirit"));
         video1.AddComment(new Comment("Lucy", "I'm grateful for the Prophet Joseph Smith"));
 
-        Video video2 = new Video("Jesus Christ: Our Savior", "ComeUntoChrist.org", 360);
+        Video video2 = new Video("Jesus Christ: Our Savior", "The Church of Jesus Christ of Latter-day Saints", 360);
         video2.AddComment(new Comment("John", "He truly is the Light of the World"));
         video2.AddComment(new Comment("Mary", "This strengthened my testimony"));
         video2.AddComment(new Comment("Nephi", "Powerful message about the Atonement"));
 
-        Video video3 = new Video("Temples: Houses of the Lord", "Latter-day Saints Channel", 510);
+        Video video3 = new Video("Temples: Houses of the Lord", "The Church of Jesus Christ of Latter-day Saints", 510);
         video3.AddComment(new Comment("Sarah", "I feel peace every time I go to the temple"));
         video3.AddComment(new Comment("Alma", "I can't wait for my endowment"));
         video3.AddComment(new Comment("Helaman", "The temple is truly the Lord's house"));
