c0c66b5d17832e0819a9b6ccc25611bbe9219b50
thneri95 <tiago.neri1995@outlook.com>
2025-05-31 17:33:12 -0300

    w05 inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // base
+        Assignment a1 = new Assignment("Tiago Borges", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // derived class
+        MathAssignment a2 = new MathAssignment("Miguel Neri", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
